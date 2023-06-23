9f2c2d26a931969a83395a3e567a0d777fec11e8
jnrsai24 <jnrsai24@gmail.com>
2023-06-23 08:32:07 +0000

    all test cases passed but still css is pending

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,48 @@
 // Write your code here.
+import {Component} from 'react'
+import './index.css'
+
+// const FaqItem = props => {
+//   const {faqItem, signClick, status} = props
+//   const {id, questionText, answerText} = faqItem
+
+//   const statusUpdate = () => {
+//     signClick(id)
+//   }
+
+class FaqItem extends Component {
+  state = {status: true}
+
+  statusUpdate = () => {
+    this.setState(prev => ({status: !prev.status}))
+  }
+
+  render() {
+    const {status} = this.state
+    const {faqItem} = this.props
+    const {questionText, answerText} = faqItem
+
+    return (
+      <li>
+        <h1>{questionText}</h1>
+
+        <button type="button" onClick={this.statusUpdate}>
+          {status ? (
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
+              alt="plus"
+            />
+          ) : (
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png "
+              alt="minus"
+            />
+          )}
+        </button>
+        {status ? ' ' : <p>{answerText}</p>}
+      </li>
+    )
+  }
+}
+
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,38 @@
 // Write your code here.
+import {Component} from 'react'
+import FaqItem from '../FaqItem'
+import './index.css'
+
+class Faqs extends Component {
+  // state = {status: true}
+
+  //   signClick = id => {
+  //     console.log(id)
+  //     this.setState(prev => ({status: !prev.status}))
+  //   }
+
+  render() {
+    // const {status} = this.state
+    const {faqsList} = this.props
+
+    return (
+      <div>
+        <div>
+          <h1>FAQs</h1>
+          <ul>
+            {faqsList.map(each => (
+              <FaqItem
+                faqItem={each}
+                key={each.id}
+                // status={status}
+                signClick={this.signClick}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Faqs
